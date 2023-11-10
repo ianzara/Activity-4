3cf4acf36e99598190036e712ab1fd2b061fcd16
kimirut <kimrigodon7@gmail.com>
2023-11-10 12:40:30 +0800

    feat/insert-10-rows-appointment

--- veterinary.sql
+++ veterinary.sql
@@ -94,3 +94,16 @@ VALUES
 
 
 
+INSERT INTO appointments (appointid, animalid, appointdate, reason)
+VALUES
+(1, 1, '2023-01-05', 'Annual check-up'),
+(2, 2, '2023-01-10', 'Vaccination'),
+(3, 3, '2023-02-02', 'Injury assessment'),
+(4, 4, '2023-02-15', 'Dental cleaning'),
+(5, 5, '2023-03-05', 'Skin condition'),
+(6, 6, '2023-03-10', 'Check for fleas'),
+(7, 2, '2023-04-12', 'Vaccination'),
+(8, 8, '2023-04-18', 'Spaying/neutering'),
+(9, 9, '2023-05-02', 'Allergy treatment'),
+(10,10, '2023-05-20', 'Eye infection');
+
